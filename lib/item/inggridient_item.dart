fa92912a56136e8916adc7816bfccc4c43bddf7e
Achmad Fathullah <amedixro@gmail.com>
2023-10-12 10:55:28 +0700

    feat(widget): add function to move to another page

--- lib/item/inggridient_item.dart
+++ lib/item/inggridient_item.dart
@@ -1,3 +1,4 @@
+import "package:first_app/recipes_screen.dart";
 import "package:flutter/material.dart";
 
 class InggridientItem extends StatelessWidget {
@@ -5,28 +6,39 @@ class InggridientItem extends StatelessWidget {
   final String imageUrl;
   const InggridientItem(this.inggridient,this.imageUrl, {super.key});
 
+  void generateRecipes(BuildContext ctx){
+    Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
+      return const RecipesScreen();
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(15),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 3,
-                blurRadius: 10,
-                offset: const Offset(0, 3))
+    return InkWell(
+      onTap: () => generateRecipes(context),
+      borderRadius: BorderRadius.circular(15),
+      splashColor: Theme.of(context).primaryColor,
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 3,
+                  blurRadius: 10,
+                  offset: const Offset(0, 3))
+            ]),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(children: [
+            Image.network(imageUrl, height: 100),
+            Text(
+              inggridient,
+              style: Theme.of(context).textTheme.titleLarge,
+            )
           ]),
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(children: [
-          Image.network(imageUrl, height: 100),
-          Text(
-            inggridient,
-            style: Theme.of(context).textTheme.titleLarge,
-          )
-        ]),
+        ),
       ),
     );
   }
