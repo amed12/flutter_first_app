b3c808db6b0223ec6715e4f0b92804c1671f859d
Achmad Fathullah <amedixro@gmail.com>
2023-10-21 00:38:26 +0700

    feat(widget): add new widget for inggridient item screen

--- lib/screen/item/card_item.dart
+++ lib/screen/item/card_item.dart
@@ -1,11 +1,13 @@
-
+import 'package:first_app/models/unsplash_model.dart';
+import 'package:first_app/services/unsplash_image_api.dart';
 import 'package:flutter/material.dart';
 
 class CardItem extends StatelessWidget {
   final VoidCallback onPressed;
   final String? title;
-  final String? urlImage;
-  const CardItem({Key? key, required this.onPressed, this.urlImage, this.title})
+  final String urlImage;
+  const CardItem(
+      {Key? key, required this.onPressed, required this.urlImage, this.title})
       : super(key: key);
 
   @override
@@ -23,28 +25,42 @@ class CardItem extends StatelessWidget {
             Stack(
               children: [
                 ClipRRect(
-                  borderRadius: BorderRadius.circular(15),
-                  child: Image.network(
-                    urlImage ?? "",
-                    height: 250,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
-                ),
+                    borderRadius: BorderRadius.circular(15),
+                    child: FutureBuilder<UnsplashImage?>(
+                      future: UnsplashImageApi.loadImagesWithKeyword(title!),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return Container(
+                            width: double.infinity,
+                            height: 250,
+                            color: Colors.white24,
+                            child: const Center(child: CircularProgressIndicator()));
+                        } else if (snapshot.hasError) {
+                          return Text('Error: ${snapshot.error}');
+                        } else {
+                          final data = snapshot.data;
+                          return Image.network(
+                            data?.getSmallUrl() ?? urlImage,
+                            height: 250,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          );
+                        }
+                      },
+                    )),
                 Positioned(
                   bottom: 20,
                   right: 10,
                   child: Container(
                     width: 220,
                     color: Colors.black54,
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 5,
-                      horizontal: 20
-                    ),
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
                     child: Text(title ?? '',
-                    style: Theme.of(context).textTheme.titleLarge,
-                    softWrap: true,
-                    overflow: TextOverflow.fade),
+                        style: Theme.of(context).textTheme.titleLarge,
+                        softWrap: true,
+                        overflow: TextOverflow.fade),
                   ),
                 )
               ],
